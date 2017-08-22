69d3e20e46de0cd6ca636c336db172a7dfb8de65
Rincewind <git@magierdinge.de>
2017-08-22 21:56:51 +0200

    it works

--- red_alert.py
+++ red_alert.py
@@ -1,86 +1,177 @@
 #!/usr/bin/python3
+import time
+import struct
+import socket
+import numpy as np
+import hashlib
+import numbers
 import sys, time, struct, argparse, os, signal
-import serial
 import pyaudio
 import wave
 """
-* This is a simple script to speak to an esp8266 over UDP, which controls a red led strip.
-* While the LED strip is flashing, the "red alert" sound from Star Trek TNG is played.
+This is a simple script to speak to an esp8266 over UDP, which controls a red led strip.
+While the LED strip is flashing, the "red alert" sound from Star Trek TNG is played.
 """
+PIDFILE = '/tmp/alert.pid'
 
-class RedAlert(object):
-    def __init__(self, port_dev, port=8266, play_audio=False,
-                 audio_file=os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), 'tng_red_alert1.wav')):
+class LED(object):
+    """
+    This represents a remote controlled LED, which can have more than one color channel.
+    Communication is done over UDP sockets.
+    Every packet has a header containing the number of channels in this packet and a sha256 checksum of the payload.
+    The communication is at this time monodirectional. You never get a response from the LED controller.
+    This might result in unnoticed packet drop. Keep your packets small to avoid being dropped.
+    You can assume, that the recieving LED runs at 25 fps, so there is not much data needed.
+    Color values are sent as bytes [0-255]. You can also use floats [0.0 - 1.0] to express light values.
+    If you do so, the values are logarithmical adjusted for linear brightness perception.
+    """
+    PACKET_HEADER = '!B32s'
+    HEADER_LEN = struct.calcsize(PACKET_HEADER)
+    def __init__(self, ip, port=8266, channel=1, fps=25, bufsize=25):
+        if ip == None:
+            raise ValueError('No destination address given!')
+        self.channel = channel
+        self.ip = ip
+        self.port = port
+        self.fps = fps
+        self.bufsize = bufsize
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        self.audio = play_audio
-        if play_audio:
-            # open audio file and configure audio stream
-            with wave.open(audio_file, 'rb') as wf:
-                self.audio_file = wf.readframes(os.path.getsize(audio_file))
+    def vision_correction(self, val, b=100):
+        """
 
-            self.audio_player = pyaudio.PyAudio()
-            self.audio_stream = self.audio_player.open(
-                format=self.audio_player.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True
-            )
-        for i in range(2):
-            self.play_audio()
-            time.sleep(0.3)
+        :param val: Brightness value [0.0 - 1.0]
+        :param b: Base of potency
+        :return: Brightness value adjusted to human vision
+        """
+        val = max(min(1, val), 0)
+        return int((b**val - 1)/(b - 1) * 256)
 
-    def play_audio(self):
-        self.audio_stream.write(self.audio_file)
+    def on(self):
+        """
+        Turn on all channels
+        """
+        self.all(255)
 
-    def read_packet_number(self):
-        read = int(self.port.readline().decode('utf8'))
-        # print(self.packet_number, '=?', read)
-        return read
+    def off(self):
+        """
+        Turn off all channels
+        """
+        self.all(0)
 
-    def fade(self, value, duration):
-        end = time.time() + duration / 1000
-        self.port.write(struct.pack('!BH', value, duration))
-        if self.audio and value > self._last_value:
-            self.play_audio()
-        self.port.flush()
-        time.sleep(max(0, end - time.time()))
-        self._last_value = value
+    def _normalize_value(self, value):
+        """
+        Tries to interpret value as tuple of brightness values and does whatever is needed to do so.
+        :param value:
+        :return:
+        """
+        if isinstance(value, numbers.Number):
+            value = [value]
 
-    def play_pattern(self, pattern):
-        for value, duration in pattern:
-            # print(value, duration)
-            self.fade(value, duration)
+        # replace illegal values with 0
+        value = [(x if isinstance(x, numbers.Number) else 0) for x in value]
 
-    def close(self):
-        self.port.close()
-        if self.audio:
-            self.audio_stream.stop_stream()
-            self.audio_stream.close()
-            self.audio_player.terminate()
+        # if floats are given values are between 0.0 and 1.0
+        if any(isinstance(x, float) for x in value):
+            value = [self.vision_correction(x) for x in value]
 
+        value = [int(min(max(0, x), 255)) for x in value]
+        return value
+
+
+    def all(self, value):
+        self.value([value]*self.channel)
+
+    def value(self, *values):
+        self.send_animation([self._normalize_value(v) for v in values])
+
+    def send_animation(self, animation):
+        self.socket.sendto(self.encode_animations(self._normalize_value(d) for d in animation), (self.ip, self.port))
+
+    def play_stream(self, animation):
+        """
+        Plays an animation an waits for completion.
+        :param animation: an iterable data structure containing tuples of brightness for every channel
+        """
+        fragment = []
+
+        for d in animation:
+            d = self._normalize_value(d)
+            fragment.append(d)
+            if len(fragment) > self.bufsize:
+                animations = list(zip(*fragment))
+                self.send_animation(*animations)
+                time.sleep(self.bufsize / self.fps)
+                fragment.clear()
+
+
+
+    def encode_animations(self, animation):
+        """
+        Forges a packet containing the animation
+        :param animation: An animation containing brightness values as tuple of integers [0 - 255]
+        :return: a byte buffer, which can be send to the LED controller
+        """
+        animation = list(animation)
+        channel_count = len(animation[0])
+        animation_len = len(animation)
+        buffer = bytearray(LED.HEADER_LEN + channel_count * animation_len)
+
+        for i in range(animation_len):
+            for c in range(channel_count):
+                buffer[LED.HEADER_LEN + i * channel_count + c] = animation[i][c]
+
+        # compute hash
+        buffer[0:LED.HEADER_LEN] = struct.pack(LED.PACKET_HEADER, channel_count, hashlib.sha256(buffer[LED.HEADER_LEN:]).digest())
+        return buffer
+
+    def __exit__(self):
+        self.socket.close()
+
+class AudioFile(object):
+    def __init__(self, audio_file):
+        with wave.open(audio_file, 'rb') as wf:
+            self.audio_data = wf.readframes(os.path.getsize(audio_file))
+
+        self.duration = wf.getnframes() / wf.getframerate()
+        audio_player = pyaudio.PyAudio()
+
+        self.audio_stream = audio_player.open(
+            format=audio_player.get_format_from_width(wf.getsampwidth()),
+            channels=wf.getnchannels(),
+            rate=wf.getframerate(),
+            output=True
+        )
+
+
+    def play(self):
+        self.audio_stream.write(self.audio_data)
 
 def already_running():
+    """
+    Looks if this programm is already running as another process
+    :return:
+    """
     running = False
-    if os.path.isfile(pidfile):
+    if os.path.isfile(PIDFILE):
         running = True
         try:
-            with open(pidfile, 'r') as f:
+            with open(PIDFILE, 'r') as f:
                 os.kill(int(f.read()), signal.SIGINT)
         except:
             running = False
-            os.remove(pidfile)
+            os.remove(PIDFILE)
 
-    with open(pidfile, 'w') as f:
+    with open(PIDFILE, 'w') as f:
         f.write(str(os.getpid()))
         f.flush()
 
     return running
 
-
 def main():
     parser = argparse.ArgumentParser(description='Flash my red light')
-    parser.add_argument('--tty', type=str, default=None,
-                        help='Path to UART device')
+    parser.add_argument('--dst', type=str, default=None,
+                        help='IP address of LED controller')
 
     parser.add_argument('--loop', type=int, default=1,
                         help='How many times should the pattern be played')
@@ -91,8 +182,6 @@ def main():
     parser.add_argument('--toggle', action='store_true',
                         help='Stop if alarm is already running')
 
-    parser.add_argument('pattern', metavar='N', type=int, nargs='+',
-                        help='flash pattern')
     args = parser.parse_args()
 
 
@@ -101,31 +190,26 @@ def main():
     if args.toggle and running:
         exit(1)
 
-    red_alert = RedAlert(args.tty, 9600, play_audio=args.audio)
-    pattern = list(zip(args.pattern[0::2], args.pattern[1::2]))
+    led = LED(args.dst)
+    audio_file = AudioFile(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), 'tng_red_alert2.wav'))
+    animation = ((np.sin(np.linspace(-np.pi / 2, 2 * np.pi - np.pi / 2, audio_file.duration * led.fps)) + 1) / 2)
 
-    def quit(*args):
-        try:
-            red_alert.fade(0, 1)
-            red_alert.close()
-        except:
-            ...
-        exit(0)
+    def run():
+        led.send_animation(animation)
+        if args.audio:
+            audio_file.play()
 
-    signal.signal(signal.SIGINT, quit)
+    for _ in range(args.loop):
+        run()
 
-    try:
-        if args.loop == 0:
-            while True:
-                red_alert.play_pattern(pattern)
-        else:
-            for i in range(args.loop):
-                red_alert.play_pattern(pattern)
-    except KeyboardInterrupt as e:
-        ...
-    quit()
+    if args.loop == 0:
+        while True:
+            run()
 
 
-pidfile = '/tmp/alert.pid'
+
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        ...
--- send_lightswitch.py
+++ send_lightswitch.py
@@ -3,35 +3,116 @@ import struct
 import socket
 import numpy as np
 import hashlib
+import numbers
 
 DST = "192.168.178.32"
 PORT = 8266
 PACKET_HEADER = '!B32s'
 HEADER_LEN = struct.calcsize(PACKET_HEADER)
 
+class LED(object):
+    def __init__(self, ip, port=8266, channel=1, fps=25, bufsize=25):
+        self.channel = channel
+        self.ip = ip
+        self.port = port
+        self.fps = fps
+        self.bufsize = bufsize
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def encode_animations(*animations):
-    animation_count = len(animations)
-    animation_len = max([len(animation) for animation in animations])
-    buffer = bytearray(HEADER_LEN + sum([len(fragment) for fragment in animations]))
+    def vision_correction(self, val, b=100):
+        # 0.0 <= val <= 1.0
+        val = max(min(1, val), 0)
+        return int((b**val - 1)/(b - 1) * 256)
 
-    for i in range(animation_len):
-        for j, fragment in enumerate(list(animations)):
-            buffer[HEADER_LEN + i * animation_count + j] = fragment[min(len(fragment), i)]
+    def on(self):
+        self.all(255)
 
-    # compute hash
-    buffer[0:HEADER_LEN] = struct.pack(PACKET_HEADER, animation_count, hashlib.sha256(buffer[HEADER_LEN:]).digest())
-    print(buffer)
-    return buffer
+    def off(self):
+        self.all(0)
 
-def send_animations(*fragments):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(encode_animations(*fragments), (DST, PORT))
-    sock.close()
+    def normalize_value(self, value):
+        if isinstance(value, numbers.Number):
+            value = [value]
 
-while True:
-    animation = ((np.sin(np.linspace(0, 2*np.pi, 50)) + 1) * 128).astype(np.uint8).tobytes()
-    send_animations(animation, animation, animation)
-    time.sleep(max(len(animation) / 25 - 0.1, 0.01))
+        # replace illegal values with 0
+        value = [(x if isinstance(x, numbers.Number) else 0) for x in value]
 
-#send_animation(b'\x00')
+        # if floats are given values are between 0.0 and 1.0
+        if any(isinstance(x, float) for x in value):
+            value = [self.vision_correction(x) for x in value]
+
+        value = [int(min(max(0, x), 255)) for x in value]
+        return value
+
+
+    def all(self, value):
+        self.value(*[value]*self.channel)
+
+    def value(self, *values):
+        self.send_animation(*[self.normalize_value(v) for v in values])
+
+    def send_animation(self, *animations):
+        self.socket.sendto(self.encode_animations(*animations), (self.ip, self.port))
+
+    def play_stream(self, data):
+        fragment = []
+
+        for d in data:
+            d = self.normalize_value(d)
+            fragment.append(d)
+            if len(fragment) > self.bufsize:
+                animations = list(zip(*fragment))
+                self.send_animation(*animations)
+                time.sleep(self.bufsize / self.fps)
+                fragment.clear()
+
+
+
+    def encode_animations(self, *animations):
+        animation_count = len(animations)
+        animation_len = max([len(animation) for animation in animations])
+        buffer = bytearray(HEADER_LEN + sum([len(fragment) for fragment in animations]))
+
+        for i in range(animation_len):
+            for j, fragment in enumerate(list(animations)):
+                buffer[HEADER_LEN + i * animation_count + j] = fragment[min(len(fragment), i)]
+
+        # compute hash
+        buffer[0:HEADER_LEN] = struct.pack(PACKET_HEADER, animation_count, hashlib.sha256(buffer[HEADER_LEN:]).digest())
+        return buffer
+
+    def __exit__(self):
+        self.socket.close()
+
+
+def sine():
+    t = 0
+    while True:
+        yield np.sin(2 * np.pi * t / 25 - np.pi / 2)/2 + 1
+        t += 1
+
+def triangle():
+    t = 0
+    while True:
+        d = abs((t % 200)-100) / 100
+        yield d
+        t += 1
+
+
+led = LED(DST)
+#led.on()
+#time.sleep(0.5)
+#led.off()
+#led.play_stream(sine())
+
+for x in np.linspace(0, 1, 100):
+    print(x)
+    led.all(x)
+    time.sleep(0.1)
+
+#while True:
+#    animation = (-(np.cos(np.linspace(0, 2*np.pi, 50)) + 1) * 128).astype(np.uint8).tobytes()
+#    send_animations(animation, animation, animation)
+#    time.sleep(max(len(animation) / 25 - 0.1, 0.01))
+
+#send_animations(b'\x00')
